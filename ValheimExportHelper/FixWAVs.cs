7fc4dafb362b069bdd45b490d4aff57e1fcb91c4
Adam Heinermann <aheinerm@gmail.com>
2023-03-15 21:29:03 -0700

    #23 Fix corrupted wav files by detection

--- ValheimExportHelper/FixWAVs.cs
+++ ValheimExportHelper/FixWAVs.cs
@@ -5,9 +5,7 @@
     public override void Export()
     {
       LogInfo("Fixing corrupted WAV files");
-
-      string boilLoopWavPath = Path.Join(AssetsPath, "AudioClip", "Water_BoilLoop.wav");
-      FixWAVFile(boilLoopWavPath);
+      FixAllWAVFiles();
     }
 
     private void FixWAVFile(string filename)
@@ -15,12 +13,28 @@
       if (File.Exists(filename))
       {
         byte[] wavFile = File.ReadAllBytes(filename);
-
         int len = wavFile.Length;
+
+        if (len < 44) return; // Assume it's unfixable
+        uint riff_size = BitConverter.ToUInt32(wavFile, 0x04);
+        uint data_size = BitConverter.ToUInt32(wavFile, 0x2A);
+
+        if (riff_size != 0 && data_size != 0) return; // Assume it doesn't need fixing
+
         BitConverter.GetBytes(len - 0x08).CopyTo(wavFile, 0x04);
         BitConverter.GetBytes(len - 0x2E).CopyTo(wavFile, 0x2A);
 
         File.WriteAllBytes(filename, wavFile);
+        LogInfo($"Fixing WAV: {filename}");
+      }
+    }
+
+    private void FixAllWAVFiles()
+    {
+      var wavFiles = Directory.EnumerateFiles(AssetsPath, "*.wav", SearchOption.AllDirectories);
+      foreach (var file in wavFiles)
+      {
+        FixWAVFile(file);
       }
     }
   }
